fb7d3b04dfe524e533f96ae79b752000ef4c6374
Slobodan Ilic <slobodan@crunch.io>
2021-03-16 23:02:00 +0100

    rfctr: sprinkle docstrings in expr.py

--- take-home/expr.py
+++ take-home/expr.py
@@ -3,31 +3,46 @@ import operator
 
 
 class Expr:
+    """Base class for all other expressions."""
+
     def eval(self, rec):
-        raise ValueError("Must subclass")
+        """return True if condition is satisfied on a given record, False otherwise."""
+        raise NotImplementedError("Each subclass must implement `eval` method.")
 
 
 class NAryExpr(Expr):
-    def __init__(self, op, nodes):
-        self._op = op
-        self._nodes = nodes
+    """Implementation of the n-ary operation (OR, AND).
+
+    These expressions can have multiple operands, and are evaluated to a single bool.
+    """
+
+    def __init__(self, op_str, operands):
+        self._op_str = op_str
+        self._operands = operands
 
     @property
     def op(self):
-        return {"OR": operator.or_, "AND": operator.and_}[self._op]
+        return {"OR": operator.or_, "AND": operator.and_}[self._op_str]
 
     def eval(self, rec):
-        return reduce(self.op, [n.eval(rec) for n in self._nodes])
+        """return True if condition is satisfied on a given record, False otherwise."""
+        return reduce(self.op, [n.eval(rec) for n in self._operands])
 
 
 class BinaryExpr(Expr):
-    def __init__(self, op, field, val):
-        self._op = op
+    """Implementation of the binary op (<, >, <=, >=, =)."""
+
+    def __init__(self, op_str, field, val):
+        self._op_str = op_str
         self._field = field
         self._val = val
 
     @property
     def op(self):
+        """operator from python operator module, based on operation name (str).
+
+        This operator will be used for expression evaluation.
+        """
         return {
             "<": operator.lt,
             "<=": operator.le,
@@ -35,23 +50,31 @@ class BinaryExpr(Expr):
             ">=": operator.ge,
             "=": operator.eq,
             "LIKE": operator.contains,
-        }[self._op]
+        }[self._op_str]
 
     def eval(self, rec):
+        """return True if condition is satisfied on a given record, False otherwise."""
         return self.op(self._field_val(rec), self.val)
 
     @property
     def val(self):
+        """int value of right-hand side expression, if convertible, str otherwise."""
         try:
             return int(self._val)
         except ValueError:
             return self._val
 
     def _field_val(self, rec):
+        """return dictionary entry from the input table record, based on field name."""
         return rec[self._field]
 
 
 class LiteralExpr(Expr):
+    """Implementation of single value literals like True and False.
+
+    The class serves to enable generic evaluation of the expressions with `eval`.
+    """
+
     def __init__(self, val):
         self._val = val
 
